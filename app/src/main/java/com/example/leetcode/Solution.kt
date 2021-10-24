08fd98171c7ec6e81e3825d4f8d36b80f7461eea
Michael Stevens <michaeljstevens443@gmail.com>
2021-10-24 15:01:42 -0400

    Increment/Decrementer solution & running sum

--- app/src/main/java/com/example/leetcode/Solution.kt
+++ app/src/main/java/com/example/leetcode/Solution.kt
@@ -66,3 +66,28 @@ fun improvedRunningSums(nums: IntArray): IntArray {
     }
     return nums;
 }
+
+/*
+There is a programming language with only four operations and one variable X:
+    ++X and X++ increments the value of the variable X by 1.
+    --X and X-- decrements the value of the variable X by 1.
+Initially, the value of X is 0.
+Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.
+ */
+fun finalValueAfterOperations(operations: Array<String>): Int {
+    var x = 0
+    loop@ for (word in operations) {
+        when (word) {
+            "++X" -> ++x
+            "X++" -> x++
+            "--X" -> --x
+            "X--" -> x--
+            else -> {
+                println("Invalid input")
+                x = 0
+                break@loop
+            }
+        }
+    }
+    return x
+}
