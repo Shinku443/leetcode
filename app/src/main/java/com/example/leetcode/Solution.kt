735ed00cf08969698f175aff91a67ab7b965d994
Michael Stevens <michaeljstevens443@gmail.com>
2021-10-26 19:54:14 -0400

    Solutions for tree (COME BACK AND REVIEW)

--- app/src/main/java/com/example/leetcode/Solution.kt
+++ app/src/main/java/com/example/leetcode/Solution.kt
@@ -1,20 +1,25 @@
 package com.example.leetcode
 
 class Solution {
-    fun twoSum(nums: IntArray, target: Int): IntArray {
-        for (index in nums.indices) {
-            for (index2 in index + 1 until nums.size) {
-                if (nums[index] + nums[index2] == target) {
-                    return intArrayOf(index, index2)
-                }
-            }
-        }
-        return IntArray(0)
+    fun test() {
+
     }
 }
 
-//Better Solution
 fun twoSum(nums: IntArray, target: Int): IntArray {
+    for (index in nums.indices) {
+        for (index2 in index + 1 until nums.size) {
+            if (nums[index] + nums[index2] == target) {
+                return intArrayOf(index, index2)
+            }
+        }
+    }
+    return IntArray(0)
+}
+
+
+//Better Solution
+fun twoSumBetter(nums: IntArray, target: Int): IntArray {
 
     val indexedList = nums.withIndex().toList()
     indexedList.forEach { (indexA, valueA) ->
@@ -140,9 +145,56 @@ fun removeElement(nums: IntArray, `val`: Int): Int {
     return found
 }
 
+/*
+fun permute(nums: IntArray): List<List<Int>> {
+
+}*/
+
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
+
+//TODO COME BACK TO THIS
+fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
+    if (p?.`val` == null && q?.`val` == null) {
+        return true
+    }
+    if (p?.`val` != null && q?.`val` == null) {
+        return false
+    }//these can be reduced to just p == null || q == null
+    if (q?.`val` != null && p?.`val` == null) {
+        return false
+    }
+    if (p?.`val` != q?.`val`) {
+        return false
+    }
+    return isSameTree(p?.left, q?.left) && isSameTree(p?.right, q?.right)
+}
+
+fun isSameTreeImproved(p: TreeNode?, q: TreeNode?): Boolean {
+    if (p === q) return true
+    if (p?.`val` != q?.`val`) return false
+    return isSameTree(p?.left, q?.left) && isSameTree(p?.right, q?.right)
+}
+
+//TODO - COMEBACK FOR THIS TOO
+fun invertTree(root: TreeNode?): TreeNode? {
+    println("NEW INVERSION root: ${root?.`val`}")
+    if (root == null) {
+        println("root is null")
+        return null
+    }
+    invertTree(root.left)
+    invertTree(root.right)
+    var tmp: TreeNode? = root.left
+    root.left = root.right
+    root.right = tmp
+
+    return root
+}
 
 fun main() {
-    val sol: Solution = Solution()
     val nums = intArrayOf(2, 0, 4, 3, 1)
     val buildNums = intArrayOf(0, 2, 1, 5, 3, 4)
     val increasingNums = intArrayOf(1, 2, 3, 4, 5)
@@ -152,8 +204,27 @@ fun main() {
     val removalTarget = 2
     val shuffleTarget = 3
     val target = 6
+    //Tree Area
+    val pTree = TreeNode(1)
+    pTree.left = TreeNode(2)
+    //pTree.right = TreeNode(3)
+    pTree.right = null
+    val qTree = TreeNode(1)
+    qTree.left = TreeNode(2)
+    //qTree.right = TreeNode(3)
+    qTree.right = null
+    //Invert Tree
+    val invertingTree = TreeNode(4)
+    val invertLeft = TreeNode(2)
+    invertingTree.left = invertLeft
+    invertLeft.left = TreeNode(1)
+    invertLeft.right = TreeNode(3)
+    val invertRight = TreeNode(7)
+    invertingTree.right = invertRight
+    invertRight.left = TreeNode(6)
+    invertRight.right = TreeNode(9)
 
-    println("Two Sums: " + sol.twoSum(nums, target))
+    println("Two Sums: " + twoSum(nums, target))
     println("Build Array: " + buildArray(buildNums).contentToString())
     println("Increasing Array: " + improvedRunningSums(increasingNums).contentToString())
     println("Shuffling: " + shuffle(shuffleNums, shuffleTarget).contentToString())
@@ -167,4 +238,10 @@ fun main() {
     println("Numbering Identical Pairs: ${numIdenticalPairs(goodPairNums)}")
 
     println("Removing Numbers: ${removeElement(removalNums, removalTarget)}")
+
+    println("Is Same Tree: ${isSameTree(pTree, qTree)}")
+
+    println("Is Same Tree Improved: ${isSameTreeImproved(pTree, qTree)}")
+    println("Inverting Tree: ${invertTree(invertingTree)?.`val`}")
 }
+
